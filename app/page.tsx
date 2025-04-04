e62ccd7cd78467c07395eea0dbd2e14eba71279b
Kelvin <agbakpeykelvin@gmail.com>
2025-04-04 05:48:51 +0000

    added ad carousel/hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,50 @@
+import {
+  Carousel,
+  CarouselContent,
+  // CarouselItem,
+  // CarouselNext,
+  // CarouselPrevious,
+} from "@/components/ui/carousel"
+
+
+
 const Home = () => {
+  const sliderImages = [
+    { src: '/slider-images/5-cedis-per-delivery.png', alt: 'image 1' },
+    { src: '/slider-images/Free-Delivery.png', alt: 'image 2' },
+    { src: '/slider-images/How-to-order.png', alt: 'image 3' }
+  ]
+
+
   return (
     <main className='w-[93%] mx-auto min-h-screen'> {/* min-h-screen */}
-      <h2 className='text-4xl'>Home</h2>
-  
+
+
+        
+        {/* ad carousel */}
+        <section className="h-[40%]">
+          <Carousel images={sliderImages} opts={{
+            align: 'start',
+            loop: true,
+          }}> 
+            <CarouselContent />
+          </Carousel>
+        </section>
+
+
+        <section>
+        
+        </section>
+
+      
+
+
+
+
+
     </main>
   )
 }
+
 
 export default Home
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,5 @@
 'use client'
-import { HouseIcon } from "lucide-react"
-import Link from "next/link"
+import { FaHouse } from 'react-icons/fa6'
 
 
 const Navbar = () => {
@@ -8,15 +7,16 @@ const Navbar = () => {
           <header className="mx-auto w-[93%] h-16 flex items-center">
                <div className="flex space-x-2">
                     <section>
-                         <div className="p-1.5 rounded-full bg-primary">
-                              <HouseIcon className="rounded-full fill-primary-pale stroke-transparent size-5" />
+                         <div className="p-[7px] rounded-full bg-primary">
+                              {/* <House className="size-5 stroke-primary-pale" /> */}
+                              <FaHouse className="size-[18px]" />
                          </div>
                     </section>
 
                     {/* location */}
                     <section className="flex items-center space-x-0.5">
                          <span className="font-semibold">Home,</span>
-                         <span>University of Ghana</span>
+                         <span className="pl-0.5">University of Ghana</span>
                     </section>
                </div>
           </header>
