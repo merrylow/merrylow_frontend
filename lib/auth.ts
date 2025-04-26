0479ec99759b9419f74201cc465e0bc59b5e5d2a
Kelvin <agbakpeykelvin@gmail.com>
2025-04-26 13:06:15 +0000

    check

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,10 +5,7 @@ import Nodemailer from 'next-auth/providers/nodemailer'
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
         Google,
-        // Nodemailer({
-        //     server: process.env.EMAIL_SERVER,
-        //     from: process.env.EMAIL_FROM,
-        // }),
+
     ],
     pages: {
         signIn: "/profile",
